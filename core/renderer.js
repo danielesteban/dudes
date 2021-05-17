cb1233bbc7e02eb56253b2514f57a34a74b0725e
Daniel Esteban Nombela <dani@gatunes.com>
2021-05-17 12:36:30 +0200

    Tweak again

--- core/renderer.js
+++ core/renderer.js
@@ -19,7 +19,7 @@ class Renderer {
     };
     this.dom = dom;
 
-    this.camera = new PerspectiveCamera(70, 1, 0.1, 1000);
+    this.camera = new PerspectiveCamera(70, 1, 0.1, 2000);
     this.camera.position.y = 1.6;
 
     this.renderer = new WebGLRenderer({
